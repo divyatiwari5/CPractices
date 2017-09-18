e83073de2cda49adf4307db8c0397d6b524166b6
divya <tiwari.divya50@gmail.com>
2017-09-18 14:51:13 +0530

    Corrected for loop

--- count_argc.c
+++ count_argc.c
@@ -2,7 +2,7 @@
 
 void main(int argc, char *argv[]){
     printf("Total no. of arguments passed: %d \n", argc);
-    for(int i=0; i<=argc; i++){
-        printf("%d. %s\n", i, argv[i]);    	
-    }	
+    for(int i=0; i<argc; i++){
+        printf("%d. %s\n", i, argv[i]);
+    }
 }
